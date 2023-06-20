40e67f58b96339507e3b84e92801602783c11242
samuel <samuelkelvinmwangi@gmail.com>
2023-06-20 10:26:25 +0300

    adding info to the program

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+pp [{ id: 1, hello: "World" }, 
+     { id: 2, hello: "Ruby" }, 
+    { id: 3, hello: "Moon" }, 
+    { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+ puts "Hello World!"
+ puts "Hello world!"
+print "Hello world!"
+print "Hello world!"
+ print "Pass this test, please."
+ p [1,2,3]
 
 # def add
     
